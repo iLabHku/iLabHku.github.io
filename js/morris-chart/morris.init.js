0b0706397ac4b60020c43c1c2e98de6b87bd56cc
Rui Zhao <christianoray@gmail.com>
2021-03-09 14:30:42 +0800

    update

--- js/morris-chart/morris.init.js
+++ js/morris-chart/morris.init.js
@@ -1,40 +1,38 @@
-Morris.Bar({
-    element: 'graph-bar',
-    data: [
-        {x: '2011 Q1', y: 3, z: 2, a: 3},
-        {x: '2011 Q2', y: 2, z: null, a: 1},
-        {x: '2011 Q3', y: 0, z: 2, a: 4},
-        {x: '2011 Q4', y: 2, z: 4, a: 3}
-    ],
-    xkey: 'x',
-    ykeys: ['y', 'z', 'a'],
-    labels: ['Y', 'Z', 'A'],
-    barColors:['#414e62','#788ba0','#6dc5a3']
+// Morris.Bar({
+//     element: 'graph-bar',
+//     data: [
+//         {x: '2011 Q1', y: 3, z: 2, a: 3},
+//         {x: '2011 Q2', y: 2, z: null, a: 1},
+//         {x: '2011 Q3', y: 0, z: 2, a: 4},
+//         {x: '2011 Q4', y: 2, z: 4, a: 3}
+//     ],
+//     xkey: 'x',
+//     ykeys: ['y', 'z', 'a'],
+//     labels: ['Y', 'Z', 'A'],
+//     barColors:['#414e62','#788ba0','#6dc5a3']
 
 
-});
+// });
 
 
 
 var day_data = [
-    {"elapsed": "I", "value": 34},
-    {"elapsed": "II", "value": 24},
-    {"elapsed": "III", "value": 3},
-    {"elapsed": "IV", "value": 12},
-    {"elapsed": "V", "value": 13},
-    {"elapsed": "VI", "value": 22},
-    {"elapsed": "VII", "value": 5},
-    {"elapsed": "VIII", "value": 26},
-    {"elapsed": "IX", "value": 12},
-    {"elapsed": "X", "value": 19}
+    { y: '2021', a: 0, b: 0 },
+    { y: 'Apr', a: 15, b: 20 },
+    { y: 'Jul', a: 35, b: 25 },
+    { y: 'Oct', a: 50, b: 40 },
+    { y: '2022', a: 65, b: 50 },
+    { y: 'Apr', a: 80, b: 75 },
+    { y: 'Jul', a: 85, b: 80 },
+    { y: 'Oct', a: 90, b: 85 }
 ];
 Morris.Line({
     element: 'graph-line',
     data: day_data,
-    xkey: 'elapsed',
-    ykeys: ['value'],
-    labels: ['value'],
-    lineColors:['#1FB5AD'],
+    xkey: 'y',
+    ykeys: ['a','b'],
+    labels: ['Plan Progress','Actual progress'],
+    lineColors:['#1FB5AD','#188ae2'],
     parseTime: false
 });
 
@@ -46,22 +44,38 @@ Morris.Area({
     element: 'graph-area-line',
     behaveLikeLine: false,
     data: [
-        {x: '2011 Q1', y: 3, z: 3},
-        {x: '2011 Q2', y: 2, z: 1},
-        {x: '2011 Q3', y: 2, z: 4},
-        {x: '2011 Q4', y: 3, z: 3},
-        {x: '2011 Q5', y: 3, z: 4}
+        {x: '2021 Q1', y: 100, z: 88},
+        {x: '2021 Q2', y: 120, z: 100},
+        {x: '2021 Q3', y: 140, z: 110},
+        {x: '2021 Q4', y: 130, z: 89},
+        {x: '2022 Q1', y: 110, z: 100},
+        {x: '2022 Q2', y: 150, z: 135},
+        {x: '2022 Q3', y: 125, z: 121},
+        {x: '2022 Q4', y: 133, z: 130}
     ],
     xkey: 'x',
     ykeys: ['y', 'z'],
-    labels: ['Y', 'Z'],
+    labels: ['Finished parts', 'Qualified quality'],
     lineColors:['#414e62','#6dc5a3']
 
 
 
 });
-
-
+// Use Morris.Area instead of Morris.Line
+Morris.Donut({
+    element: 'graph-donut-passingRate',
+    data: [
+        {value: 90, label: '90%', formatted: 'Passed' },
+        {value: 5, label: '5%', formatted: 'Pending' },
+        {value: 5, label: '5%', formatted: 'Error' }
+    ],
+    backgroundColor: '#fff',
+    labelColor: '#6dc5a3',
+    colors: [
+        "#6dc5a3", "#CCCCCC", "#414e62"
+    ],
+    formatter: function (x, data) { return data.formatted; }
+});
 
 
 
@@ -69,10 +83,10 @@ Morris.Area({
 Morris.Donut({
     element: 'graph-donut',
     data: [
-        {value: 70, label: 'foo', formatted: 'at least 70%' },
-        {value: 15, label: 'bar', formatted: 'approx. 15%' },
-        {value: 10, label: 'baz', formatted: 'approx. 10%' },
-        {value: 5, label: 'A really really long label', formatted: 'at most 5%' }
+        {value: 70, label: '70%', formatted: 'Latest Completion Rate' },
+        {value: 15, label: '15%', formatted: 'Pending verification' },
+        {value: 10, label: '10%', formatted: 'Construction in progress' },
+        {value: 5, label: '5%', formatted: 'Waiting to be completed' }
     ],
     backgroundColor: '#fff',
     labelColor: '#1fb5ac',
@@ -115,6 +129,8 @@ Morris.Area({
     hideHover: 'auto'
 
 });
+
+
 
 
 
